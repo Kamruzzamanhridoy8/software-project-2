e4d6bd5e1c0c731ee8249da77735e5d828e09b83
Kamruzzamanhridoy8 <Kamruzzamanhridoy@hotmail.com>
2022-12-20 13:02:40 +0600

    finishing the finctionality of js add alert service when the fild is empty

--- js/action.js
+++ js/action.js
@@ -4,15 +4,20 @@ function insersion() {
     var in3 = document.getElementById('in3').value;
     var in4 = document.getElementById('in4').value;
 
-    var outputTable =document.getElementById('outputTable');
-    var row = outputTable.insertRow(1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
 
-    cell1.innerHTML = in1;
-    cell2.innerHTML = in2;
-    cell3.innerHTML = in3;
-    cell4.innerHTML = in4;
+    if (in1==''|| in2==''|| in3==''|| in4=='') {
+        alert("the fild is empty please fill the fild");
+    }else{
+
+        var outputTable =document.getElementById('outputTable');
+        var row = outputTable.insertRow(1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        cell1.innerHTML = in1;
+        cell2.innerHTML = in2;
+        cell3.innerHTML = in3;
+        cell4.innerHTML = in4;
+    }
 }
